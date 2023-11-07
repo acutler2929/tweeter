baaf4525a49c476fc18e8f3c9336ac60250e5254
Alice Cutler <acutler6733@mail.kvcc.edu>
2023-11-06 19:49:14 -0500

    add tests for home page url and view

--- tweeter/tests.py
+++ tweeter/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(SimpleTestCase):
+    """Home Page Tests"""
+
+    def test_url_exists_at_correct_location(self):
+        """Test url exists at correct location"""
+
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view(self):
+        """Test Home Page View"""
+
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "Home")
+
